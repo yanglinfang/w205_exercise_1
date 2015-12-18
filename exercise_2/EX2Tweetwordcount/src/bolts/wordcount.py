2b84c12c717b384aa8b1e0a94528812439f815b1
Lin <yanglinfang@ischool.berkeley.edu>
2015-12-18 08:01:39 +0800

    update

--- exercise_2/EX2Tweetwordcount/src/bolts/wordcount.py
+++ exercise_2/EX2Tweetwordcount/src/bolts/wordcount.py
@@ -3,20 +3,21 @@ from __future__ import absolute_import, print_function, unicode_literals
 from collections import Counter
 from streamparse.bolt import Bolt
 
+import psycopg2
 
 
 class WordCounter(Bolt):
 
     def initialize(self, conf, ctx):
         self.counts = Counter()
-       
+        self.conn = psycopg2.connect(database="postgres", user="postgres", password="pass", host="localhost", port="5432")
 
     def process(self, tup):
         word = tup.values[0]
 
         # Write codes to increment the word count in Postgres
         # Use psycopg to interact with Postgres
-        # Database name: Tcount 
+        # Database name: Postgres 
         # Table name: Tweetwordcount 
         # you need to create both the database and the table in advance.
         
